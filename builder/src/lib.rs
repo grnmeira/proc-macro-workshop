d19cb09d5cb3d0be463a84ef24390fa1f5030140
Gustavo Meira <grnmeira@gmail.com>
2023-05-27 21:59:55 +0100

    WIP

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,9 +1,23 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
+    let derive_input = parse_macro_input!(input as DeriveInput);
+    let original_struct_ident = &derive_input.ident;
+    let builder_struct_name = std::format!("{}{}", derive_input.ident, "Builder");
+    let builder_struct_ident = syn::Ident::new(&builder_struct_name, Span::call_site());
 
-    TokenStream::new()
+    let expanded = quote! {
+        impl #original_struct_ident {
+            fn builder() {}
+        }
+        struct #builder_struct_ident {
+
+        }
+    };
+
+    TokenStream::from(expanded)
 }
