885070bc5d8879570774733fc7ca0303b0c03678
Gustavo Meira <grnmeira@gmail.com>
2023-05-29 22:07:31 +0100

    finishing up test 2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, punctuated::Punctuated, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -10,12 +10,41 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_struct_name = std::format!("{}{}", derive_input.ident, "Builder");
     let builder_struct_ident = syn::Ident::new(&builder_struct_name, Span::call_site());
 
+    let named_fields = match derive_input.data {
+        Data::Struct(data_struct) => match data_struct.fields {
+            Fields::Named(named_fields) => named_fields.named,
+            _ => Punctuated::new(),
+        },
+        _ => Punctuated::new(),
+    };
+
+    let builder_fields: Vec<proc_macro2::TokenStream> = named_fields
+        .iter()
+        .map(|f| {
+            let ident = &f.ident;
+            let ty = &f.ty;
+            quote! { #ident: Option<#ty> }
+        })
+        .collect();
+
+    let constructor_fields: Vec<proc_macro2::TokenStream> = named_fields
+        .iter()
+        .map(|f| {
+            let ident = &f.ident;
+            quote! { #ident: None }
+        })
+        .collect();
+
     let expanded = quote! {
         impl #original_struct_ident {
-            fn builder() {}
+            fn builder() {
+                #builder_struct_ident {
+                    #( #constructor_fields ),*
+                }
+            }
         }
-        struct #builder_struct_ident {
-
+        pub struct #builder_struct_ident {
+            #( #builder_fields ),*
         }
     };
 
